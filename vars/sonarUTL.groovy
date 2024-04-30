237bf95ef08c14090fcdd4ff0fe7f14b94c46272
naman <namansingh48@gmail.com>
2024-04-30 16:59:18 +0530

    added sonar quality gate step

---
+++ vars/sonarUTL.groovy
@@ -0,0 +1,20 @@
+def call(httpUrl, username, password) {
+def qualityGateProjectStatus = httpRequest(
+url: httpUrl,
+authentication: 'USERNAME_PASSWORD',
+username: sonar,
+password: sonar
+).content
+def qualityGate = new groovy.json.JsonSlurper().parseText(qualityGateProjectStatus)
+if (qualityGate.status.condition.error) {
+// Quality Gate failed, check if it's due to code coverage
+if (
+qualityGate.status.condition.name == 'Coverage') {
+error "Code Coverage failed Quality Gate! Coverage below 60%"
+} else {
+// Quality Gate failed due to other reasons
+error "SonarQube Quality Gate failed! See SonarQube for details"
+}
+}
+// Your logic to check and fail based on Quality Gate status
+}
