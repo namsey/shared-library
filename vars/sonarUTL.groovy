044641548e362727e50a685920f7185f22676f2f
NAMAN SINGH <namansingh48@gmail.com>
2024-05-03 05:31:44 +0530

    Update sonarUTL.groovy

--- vars/sonarUTL.groovy
+++ vars/sonarUTL.groovy
@@ -1,22 +1,12 @@
-def call(gateName) {
-// Access SonarQube server details from Jenkins credentials
-def sonarqubeUrl = credentials('http://192.168.1.8:9000/')
-def sonarqubeUsername = credentials('admin')
-def sonarqubePassword = credentials('sonar')
-// Define the SonarQube API endpoint for quality gate status
-def apiUrl = "${sonarqubeUrl}/api/qualitygates/project_status?projectKey=\${JOB_NAME}&gateId=\${gateName}"
-// Use 'sh' step to execute curl command and capture response
-def response = sh(
-script: "curl -u ${sonarqubeUsername}:${sonarqubePassword} ${apiUrl}",
-returnStdout: true
-)
-// Parse the JSON response to get the quality gate status
-def jsonSlurper = new JsonSlurper()
-def data = jsonSlurper.parseText(response)
-// Check if the quality gate is not met (status != OK)
-if (data.status != 'OK') {
-error "SonarQube Quality Gate '${gateName}' failed for project '${JOB_NAME}'"
-}
-// Success if quality gate is met
-echo "SonarQube Quality Gate '${gateName}' passed for project '${JOB_NAME}'"
+def call() {
+    timeout(time: 3, unit: 'MINUTES') {
+        echo "Initializing quality gates..."
+        sh 'sleep 10' //small delay because project quality can still being published on previous stage (specially on bigger projects).  
+        def result = waitForQualityGate() //this is enabled by quality gates plugin: https://wiki.jenkins.io/display/JENKINS/Quality+Gates+Plugin
+        if (result.status != 'OK') {
+             error "Pipeline aborted due to quality gate failure: ${result.status}"
+        } else {
+             echo "Quality gate passed with result: ${result.status}"
+        }
+    }
 }
